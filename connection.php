19ef9702192f2b0ccb62a41f59c384bc6c9492b6
walugembe expedicto <54406688+Billy004@users.noreply.github.com>
2021-07-01 14:12:38 +0300

    the database has word well

--- connection.php
+++ connection.php
@@ -1,54 +1,51 @@
 <?php
-session_start();
-$hostname='localhost';
-$authname='root';
-$pass='';
-$dbname='101';
-// connecting to database
-$conn= new mysqli($hostname,$authname,$pass,$dbname);
-// checking if database is connected
-//if($conn){
-    //echo "Database connected";}
-   // else {
-   // echo "Database Not Connected";
-   //}
-   // encrypting unique variables
-   switch ($_POST['action']) {
-       case 'signup':
-        $email=$_POST['email'];
-        $password=password_hash($_POST['password'], PASSWORD_DEFAULT);
-       // echo $email."<br>".$password;
-       //inseting data into the table users in the database
-       $sql="INSERT INTO users(email,password) VALUES('$email','$password')";
-       $run_qry=mysqli_query($conn,$sql);
-       if($run_qry){
-           header("location:login.php");
-       }
-       break;
-       /*
-       case 'login':
-        $email=$_POST['email'];
-        $password=$_POST['password'];
+    session_start();
 
-        $select_user="SELECT * FROM users WHERE email='$email'";
-        $run_qry=mysqli_query($conn,$select_user);
-        if(mysqli_num_rows($run_qry)>0){
-            while ($row=mysqli_fetch_assoc($run_qry)){
-                if(password_verify($password,$row['password'])){
-                    $email=$row['email'];                    
-                    $_SESSION["email"]=$row['email'];
-                    header("location:index.php");
-                    
-                }
-                else{
-                    header("location:login.php");
-                }
-            }
-        }
-        else{
-            header("location:login.php");
-        }
-        break;*/
-   } 
+    $host = "localhost";
+    $dbuser = "root";
+    $dbpassword = "";
+    $dbname = "101";
 
+    
+    $username=$_POST['email'];
+    $password=$_POST['password'];
+
+    // database connetion
+
+    $conn = new mysqli($host,$dbuser,$dbpassword,$dbname,);
+    $link = mysqli_connect("localhost", "root", "", "101");
+
+    // Check connection
+    if($conn->connect_error){
+        die('connection failed :'.$conn->connect_error);
+    }
+
+
+    
+    // Escape user inputs for security
+      $email = mysqli_real_escape_string($link, $_REQUEST['email']);
+      $password = password_hash($_post['password'], PASSWORD_DEFAULT);
+      
+
+
+    $sql = "INSERT INTO users (email, password) VALUES ('$email','$password')";
+
+    $results = $conn-> query($sql);
+
+    if ($conn->query($sql) === TRUE) {
+        header("location:index.html");
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+      
+      //displaying
+
+      
+
+
+
+
+      // Close connection
+      mysqli_close($link);
+    
 ?>
