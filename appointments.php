b3e4229acb7d27ea4f6449ef27ffb4fd7bd95ea0
walugembe expedicto <54406688+Billy004@users.noreply.github.com>
2021-07-03 08:15:51 +0300

    table

--- appointments.php
+++ appointments.php
@@ -90,7 +90,7 @@ https://templatemo.com/tm-537-art-factory
             <div class="container">
                 <div class="row">
                     <div class="left-text col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-                        <div class ="loginbox">
+                        
                             <!-- table -->
                     <?php 
                         $host = "localhost";
@@ -135,7 +135,7 @@ https://templatemo.com/tm-537-art-factory
                         } 
                         
                         ?>
-                        </div>
+                        
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
                         <img src="assets/images/slider-icon.png" class="rounded img-fluid d-block mx-auto" alt="First Vector Graphic">
